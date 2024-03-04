1223b7872cb93101b1eaad06de8d33e9b7ec5995
Amir Badrudeen <amirbadrudeen@gmail.com>
2024-03-04 23:43:07 +0000

    Create json_lab.js

---
+++ JSON Lab/json_lab.js
@@ -0,0 +1,8 @@
+var fruit = [
+  {name:"Apple", quantity:20, color:"red"},
+	{name:"Orange", quantity:10, color:"orange"}
+  {name:"Banana", quantity:15, color:"yellow"}
+  {name:"Kiwi", quantity:5, color:"green"}  
+  {name:"Blueberry", quantity:5, color:"blue"}
+  {name:"Grapes", quantity:10, color:"purple"}
+]
