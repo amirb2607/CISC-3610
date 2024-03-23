4fdf6ccb3297e3cc9291a6d315db0b98840996e7
Amir Badrudeen <thebotmir@Amirs-PC.>
2024-03-23 14:53:58 -0400

    finish json-lab

--- JSON Lab/json_lab.js
+++ JSON Lab/json_lab.js
@@ -1,27 +1,29 @@
 var fruit = [
-    {name:"Apple", quantity:20, color:"red"},
-    {name:"Orange", quantity:10, color:"orange"},
-    {name:"Banana", quantity:15, color:"yellow"},
-    {name:"Kiwi", quantity:5, color:"green"},  
-    {name:"Blueberry", quantity:5, color:"blue"},
-    {name:"Grapes", quantity:10, color:"purple"},
-  ]
+  {name:"Apple", quantity:20, color:"red"},
+  {name:"Orange", quantity:10, color:"orange"},
+  {name:"Banana", quantity:15, color:"yellow"},
+  {name:"Kiwi", quantity:5, color:"green"},  
+  {name:"Blueberry", quantity:5, color:"blue"},
+  {name:"Grapes", quantity:10, color:"purple"},
+]
 
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
 ctx.font = "bold 16px Arial";
 ctx.textAlign = 'center';
-ctx.textBaseline = 'bottom';
+ctx.textBaseline = 'middle';
 
 for(let i = 0; i < fruit.length; i++) {
-    const name = fruit[i].name;
-    const color = fruit[i].color;
-    const quant = fruit[i].quantity;
-    ctx.fillStyle = color;
-    ctx.fillRect(0, i * 200, (quant * 25), (canvas.height / fruit.length));
-    //ctx.fillStyle = "black";
-    
-    ctx.fillText(fruit[i].name, 50, (i * 200) - 50);
-    //ctx.fillText("Red", 0 + 20, (i * 200) - 50);
+  const name = fruit[i].name;
+  const color = fruit[i].color;
+  const quant = fruit[i].quantity;
+  const barHeight = canvas.height / fruit.length;
+  
+  ctx.fillStyle = color;
+  ctx.fillRect(0, i * barHeight, quant * 25, barHeight);
+  
+  ctx.fillStyle = "black";
+  ctx.fillText(`${name}`, 50, (i * barHeight) + (barHeight / 2));
+  ctx.fillText(`${quant}`, 50, (i * barHeight) + (barHeight / 2) + 25);
 }
