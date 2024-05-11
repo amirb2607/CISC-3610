e6e3dac81ac319345f707f2b67d0cd6433a31314
Amir Badrudeen <amirbadrudeen@gmail.com>
2024-05-11 19:53:31 -0400

    Minor Fix

--- indexScript.js
+++ indexScript.js
@@ -7,7 +7,7 @@ function daysAgo(date) {
 }
 
 window.onload = function() {
-    var dates = ["2024-03-03", "2024-03-23" ,"2024-05-04" ,"2024-05-06", "2024-05-05" ,"2024-05-11"]; // Replace with your dates
+    var dates = ["2024-03-04", "2024-02-26" ,"2024-02-28" ,"2024-03-04", "2024-03-11" ,"2024-03-18" ,"2024-03-25"]; // Replace with your dates
     for (var i = 0; i < dates.length; i++) {
         var days = daysAgo(dates[i]);
         document.getElementById("daysAgo" + (i + 1)).textContent = days + " days ago";
