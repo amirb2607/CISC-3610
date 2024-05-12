d49805ad646e503af25c4ea1bc6ced29278e6e8c
Amir Badrudeen <amirbadrudeen@gmail.com>
2024-05-11 22:58:10 -0400

    Finished Data Visualization

--- Data/script.js
+++ Data/script.js
@@ -9,7 +9,7 @@ new Chart(ctx, {
     label: 'NYPD Total Arrests by Borough (as of April 22, 2024)',
     data: [2892, 13782, 14221, 15324, 17402],
       backgroundColor: '#9146FF',
-      borderColor: '#FFFFFF',
+      borderColor: '#000000',
       hoverBackgroundColor: '#a86dff',
       borderWidth: 2
     }]
@@ -27,11 +27,26 @@ new Chart(ctx, {
 new Chart(ctx2, {
     type: 'doughnut',
     data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple'],
+      labels: ['Malignant Neoplasms (Cancer)', 'Influenza (Flu) and Pneumonia', 'Diseases of Heart', 
+      'All Other Causes', 'Diabetes Mellitus', 'Cerebrovascular Disease (Stroke)', 'Chronic Lower Respiratory Diseases', 
+      'Accidents Except Drug Posioning', 'Essential Hypertension and Renal Diseases', 'Human Immunodeficiency Virus Disease (HIV)'],
       datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2],
-        borderWidth: 1
+        label: 'New York City Leading Causes of Death (as of December 11, 2023)',
+        data: [96, 96, 96, 96, 92, 90, 88, 80, 75, 45],
+        borderWidth: 1,
+        borderColor: '#000000',
+        backgroundColor: [
+            'rgb(25, 25, 112)',   // midnight blue
+            'rgb(47, 79, 79)',    // dark slate gray
+            'rgb(72, 61, 139)',   // dark slate blue
+            'rgb(85, 107, 47)',   // dark olive green
+            'rgb(128, 0, 0)',     // maroon
+            'rgb(139, 0, 139)',   // dark magenta
+            'rgb(0, 100, 0)',     // dark green
+            'rgb(100, 0, 0)',     // dark red
+            'rgb(105, 105, 105)', // dim gray
+            'rgb(0, 0, 128)'      // navy
+        ],        
       }]
     }
 });
