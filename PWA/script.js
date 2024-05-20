498c8dc6462133baf8495010a71aa87fdab076a5
Amir Badrudeen <amirbadrudeen@gmail.com>
2024-05-19 20:37:53 -0400

    Started PWA

--- PWA/script.js
+++ PWA/script.js
@@ -1,23 +1,3 @@
-var staticCacheName = "pwa";
-
-self.addEventListener("install", function (e) {
-e.waitUntil(
-	caches.open(staticCacheName).then(function (cache) {
-	return cache.addAll(["/"]);
-	})
-);
-});
-
-self.addEventListener("fetch", function (event) {
-console.log(event.request.url);
-
-event.respondWith(
-	caches.match(event.request).then(function (response) {
-	return response || fetch(event.request);
-	})
-);
-});
-
 document.getElementById('interestForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
